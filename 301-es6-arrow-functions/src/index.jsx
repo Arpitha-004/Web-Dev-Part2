969fa8532314fee7f216d9c5224937410cedf59e
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2026-06-19 01:15:05 +0530

    Js arrow functions

---
+++ 301-es6-arrow-functions/src/index.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// var numbers = [3, 56, 2, 48, 5];
+
+////Map -Create a new array by doing something with each item in an array.
+// const newNumbers = numbers.map(function (x) {
+//   return x * 2;
+// });
+// const newNumbers = numbers.map(x => x * 2);
+
+//////Filter - Create a new array by keeping the items that return true.
+// const newNumbers = numbers.filter(function(num) {
+//   return num < 10;
+// });
+// const filteredNumbers = numbers.filter(x => x < 10);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
+//     return accumulator + currentNumber;
+// })
+// const reducedNumber = numbers.reduce((accumulator, currentNumber) => accumulator + currentNumber);
+
+////Find - find the first item that matches from an array.
+// const newNumber = numbers.find(function (num) {
+//   return num > 10;
+// })
+// const foundNumber = numbers.find(x => x > 10);
+
+////FindIndex - find the index of the first item that matches.
+// const newNumber = numbers.findIndex(function (num) {
+//   return num > 10;
+// })
+// const foundIndex = numbers.findIndex(x => x > 10);
+// console.log(foundIndex);
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
